a031ebeb6036d32ec69922ab2d8246bb567df95f
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2024-09-25 14:19:18 +0200

    continuing  on bonus container

--- src/components/feber/bonus-container.tsx
+++ src/components/feber/bonus-container.tsx
@@ -6,11 +6,14 @@ interface BonusProps {
 
 export default function BonusContainer({ className }: BonusProps) {
     return (
-        <div className={className}>
+        <> 
             <h3 className={classes.h3}>LeoVegas</h3>
             <div className={classes.bonus}>2500 bonus</div>
             <div className={classes.freespins}>Casino och betting</div>
-            <div className={classes["bonus-wagering"]}></div>
-        </div>
+            <div className={classes["bonus-wagering"]}>
+                <div className={classes["bonus-turnover"]}>Omsättningskrav på bonus:</div>
+                <div className={classes["freespins-turnover"]}>Omsättningskrav på freespins:</div>
+            </div>
+        </>
     )
 }
