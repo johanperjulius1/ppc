f5223031c749718242e5126cc23d6bc4c571a551
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2025-01-18 16:20:21 +0100

    testing setting favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
 import Toplist from "@/components/toplist";
-import { Metadata } from 'next';
 
-export const metadata: Metadata = {
-  title: 'CasinoSisters.com',
-  description:
-    'CasinoSisters.com - the number one casino review page',
-};
 export default function Home (){  
   return (
     <>
