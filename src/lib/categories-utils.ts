5db9113b244fa3abccb4ac3e8db96c9fc14fc228
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2025-03-13 19:59:43 +0100

    continuing on categories

--- src/lib/categories-utils.ts
+++ src/lib/categories-utils.ts
@@ -8,3 +8,15 @@ export function getCategoriesFiles() {
     // Returns an array of file names in the directory
     return fs.readdirSync(categoriesDirectory);
 }
+
+export function getAllCategoriesData() {
+    const categoriesFiles = getCategoriesFiles();
+    const allCategoriesData = categoriesFiles.map((categoryFile) => {
+        const filePath = path.join(categoriesDirectory, categoryFile);
+        const fileContent = fs.readFileSync(filePath, "utf-8");
+        const { data, content } = matter(fileContent);
+        
+        return { data, content };
+    });
+    return allCategoriesData;
+}
