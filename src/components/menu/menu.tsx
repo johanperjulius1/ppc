0b5f11c5fed22cafe0a0c3d0d99502187c54dcd9
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2025-04-06 14:10:45 +0200

    starting menu remake

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -1,30 +1,75 @@
-"use client"
-import React, { useState } from 'react';
+import React from 'react'
 import styles from './menu.module.css';
 import Link from 'next/link';
 
-function Menu () {
-  const [isOpen, setIsOpen] = useState(false);
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
+function menu() {
   return (
-    <nav className={styles.navbar}>
-      <div className={styles['navbar-inner']}>
-        <div className={styles.menuIcon} onClick={toggleMenu}>
-          {isOpen ? '✖' : '☰'}
+    <header className={styles.header}>
+      <div className={styles.logo}>Slotsa.se</div>
+      <nav className={styles['nav-container']}>
+        <div className={styles['link-container']}>
+          <div className={styles['link-div']}><Link href="/alla-casinon">Alla Casinon</Link></div>
+          <div className={styles['link-div']}><Link href="/nya-casinon">Nya Casinon</Link></div>
+          <div className={styles['link-div']}>
+            <div className={styles['nav-button-div']}>
+              <button className={styles['nav-button']}>Casino
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  focusable="false"
+                  width="16px"
+                  height="8px"
+                  fill="white"
+                  stroke="white"
+                  stroke-width="1"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  viewBox="0 0 8 20"
+                >
+                  <path d="M11.178 19.569a.998.998 0 0 0 1.644 0l9-13A.999.999 0 0 0 21 5H3a1.002 1.002 0 0 0-.822 1.569l9 13z"></path>
+                </svg>
+              </button>
+            </div>
+          </div>
+          <div className={styles['link-div']}><Link href="/casino-med-free-spins">Free Spins</Link></div>
+          <div className={styles['link-div']}><Link href="/casino-med-bankid">Casino Med BankID</Link></div>
+          <div className={styles['link-div']}><Link href="/casino-med-swish">Casino Med Swish</Link></div>
         </div>
-        <div className={styles.logo}>Slotsa</div>
-        <ul className={`${styles.menuList} ${isOpen ? styles.showMenu : ''}`}>
-          <li><Link href="/alla-casinon">Alla Casinon</Link></li>
-          <li><Link href="/nya-casinon">Nya Casinon</Link></li>
-          <li><Link href="/casino-med-bonus">Casino Bonus</Link></li>
-          <li><Link href="/casino-med-free-spins">Free Spins</Link></li>
-          <li><Link href="/casino-med-bankid">Casino Med BankID</Link></li>
-          <li><Link href="/casino-med-swish">Casino Med Swish</Link></li>
-        </ul>
-      </div>
-    </nav>
-  );
-};
-export default Menu; 
+      </nav>
+    </header>
+  )
+}
+
+export default menu
+
+
+
+// "use client"
+// import React, { useState } from 'react';
+// import styles from './menu.module.css';
+// import Link from 'next/link';
+
+// function Menu () {
+//   const [isOpen, setIsOpen] = useState(false);
+//   const toggleMenu = () => {
+//     setIsOpen(!isOpen);
+//   };
+//   return (
+//     <nav className={styles.navbar}>
+//       <div className={styles['navbar-inner']}>
+//         <div className={styles.menuIcon} onClick={toggleMenu}>
+//           {isOpen ? '✖' : '☰'}
+//         </div>
+//         <div className={styles.logo}>Slotsa</div>
+//         <ul className={`${styles.menuList} ${isOpen ? styles.showMenu : ''}`}>
+//           <li><Link href="/alla-casinon">Alla Casinon</Link></li>
+//           <li><Link href="/nya-casinon">Nya Casinon</Link></li>
+//           <li><Link href="/casino-med-bonus">Casino Bonus</Link></li>
+//           <li><Link href="/casino-med-free-spins">Free Spins</Link></li>
+//           <li><Link href="/casino-med-bankid">Casino Med BankID</Link></li>
+//           <li><Link href="/casino-med-swish">Casino Med Swish</Link></li>
+//         </ul>
+//       </div>
+//     </nav>
+//   );
+// };
+// export default Menu; 
