bafaf1365c275196868a1732c2067ceb58c2fe6c
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2025-04-07 20:25:25 +0200

    modifying nav

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -8,10 +8,13 @@ function menu() {
       <div className={styles.logo}>Slotsa.se</div>
       <nav className={styles['nav-container']}>
         <div className={styles['link-container']}>
-          <div className={styles['link-div']}><Link href="/alla-casinon">Alla Casinon</Link></div>
-          <div className={styles['link-div']}><Link href="/nya-casinon">Nya Casinon</Link></div>
+          {/* <div className={styles['link-div']}><Link href="/alla-casinon">Alla Casinon</Link></div> */}
+          <div className={styles['link-div']}><Link href="/nya-casinon">Nya casinon</Link></div>
+          <div className={styles['link-div']}><Link href="/casino-med-bonus">Casino med bonus</Link></div>
           <div className={styles['link-div']}>
-            <div className={styles['nav-button-div']}>
+            <Link href="/casino-med-bonus-utan-omsattningskrav">Bonus utan omsättningskrav</Link>
+
+            {/* <div className={styles['nav-button-div']}>
               <button className={styles['nav-button']}>Casino
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -28,11 +31,12 @@ function menu() {
                   <path d="M11.178 19.569a.998.998 0 0 0 1.644 0l9-13A.999.999 0 0 0 21 5H3a1.002 1.002 0 0 0-.822 1.569l9 13z"></path>
                 </svg>
               </button>
-            </div>
+            </div> */}
           </div>
-          <div className={styles['link-div']}><Link href="/casino-med-free-spins">Free Spins</Link></div>
-          <div className={styles['link-div']}><Link href="/casino-med-bankid">Casino Med BankID</Link></div>
-          <div className={styles['link-div']}><Link href="/casino-med-swish">Casino Med Swish</Link></div>
+          <div className={styles['link-div']}><Link href="/casino-med-free-spins">Free spins</Link></div>
+          <div className={styles['link-div']}><Link href="/casino-med-free-spins-utan-omsattningskrav">Free spins utan omsättningskrav</Link></div>
+          <div className={styles['link-div']}><Link href="/casino-med-bankid">Casino med bankId</Link></div>
+          <div className={styles['link-div']}><Link href="/casino-med-swish">Casino med swish</Link></div>
         </div>
       </nav>
     </header>
